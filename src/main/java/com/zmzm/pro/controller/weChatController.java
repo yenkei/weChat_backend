851dfb9c27b37baf621cb5303cb0221f1fc9313a
zmzm <yenkei@126.com>
2017-12-22 14:04:13 +0800

    test

---
+++ src/main/java/com/zmzm/pro/controller/weChatController.java
@@ -0,0 +1,10 @@
+package com.zmzm.pro.controller;
+
+/**
+ * 小程序调用Controller
+ *
+ * @auther yuk
+ * @Time 2017/12/22 13:56
+ */
+public class weChatController {
+}
---
+++ src/main/java/com/zmzm/pro/service/OrderDataService.java
@@ -0,0 +1,10 @@
+package com.zmzm.pro.service;
+
+/**
+ * 订单数据相关服务
+ *
+ * @auther yuk
+ * @Time 2017/12/22 13:57
+ */
+public interface OrderDataService {
+}
